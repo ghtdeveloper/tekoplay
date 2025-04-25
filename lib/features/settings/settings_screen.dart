84c62fbf4f4eace8f51b110189abddc5141ba136
Edison Martinez <ghtdeveloper@gmail.com>
2025-04-25 11:43:39 -0400

    refactor

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -10,6 +10,7 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   String _appVersion = '';
+  double _musicVolume = 0.5;
 
   @override
   void initState() {
@@ -47,19 +48,27 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   title: 'Agregar cuenta',
                   subtitle: 'Iniciar sesión con tu cuenta',
                   icon: Icons.account_circle_outlined,
-                  onTap: () {},
+                  onTap: () => _showLoginDialog(context),
                 ),
                 _buildSettingsCard(
                   title: 'Música del juego',
                   subtitle: 'Ajusta el volumen de la música',
                   icon: Icons.music_note,
-                  onTap: () {},
+                  onTap: () {
+                    _showMusicVolumeDialog(context, _musicVolume, (newVolume) {
+                      setState(() {
+                        _musicVolume = newVolume;
+                      });
+                      // TODO Aquí podrías aplicar el volumen a tu motor de audio
+                    });
+                  },
                 ),
                 _buildSettingsCard(
                   title: 'Idioma',
                   subtitle: 'Cambiar lenguaje del juego',
                   icon: Icons.language,
-                  onTap: () {},
+                  onTap: () { _showLanguageDialog(context);
+                  },
                 ),
                 _buildSettingsCard(
                   title: 'Notificaciones',
@@ -121,3 +130,273 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 }
+
+
+void _showLoginDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      return Dialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        insetPadding: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ),
+
+              const SizedBox(height: 8),
+
+
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  'Inicio de sesión',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                elevation: 4,
+                child: ListTile(
+                  leading: const Icon(Icons.g_mobiledata, color: Colors.red, size: 32),
+                  title: const Text('Iniciar sesión con Google'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    // TODO Aquí lógica de Google
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                elevation: 4,
+                child: ListTile(
+                  leading: const Icon(Icons.apple, size: 32),
+                  title: const Text('Iniciar sesión con Apple ID'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    //TODO Aquí lógica de Apple ID
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
+void _showMusicVolumeDialog(BuildContext context, double currentVolume, Function(double) onVolumeChanged) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) {
+      double tempVolume = currentVolume;
+
+      return Dialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        insetPadding: const EdgeInsets.all(20),
+        child: StatefulBuilder(
+          builder: (context, setState) {
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'Volumen de música',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Slider(
+                    value: tempVolume,
+                    min: 0,
+                    max: 1,
+                    divisions: 10,
+                    label: '${(tempVolume * 100).round()}%',
+                    activeColor: const Color(0xFFEC7A34),
+                    inactiveColor: Colors.orange.shade100,
+                    onChanged: (value) {
+                      setState(() {
+                        tempVolume = value;
+                      });
+                    },
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        onVolumeChanged(tempVolume);
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFEC7A34),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: const Text(
+                        'Aceptar',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            );
+          },
+        ),
+      );
+    },
+  );
+}
+
+
+void _showLanguageDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false, // evita que se cierre al tocar fuera
+    builder: (BuildContext context) {
+      String selectedLanguage = 'Español'; // valor por defecto
+
+      return StatefulBuilder(
+        builder: (context, setState) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            backgroundColor: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          'Seleccionar idioma',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  RadioListTile<String>(
+                    title: const Text('Español'),
+                    value: 'Español',
+                    groupValue: selectedLanguage,
+                    activeColor: Color(0xFFEC7A34),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedLanguage = value!;
+                      });
+                    },
+                  ),
+                  RadioListTile<String>(
+                    title: const Text('Inglés'),
+                    value: 'Inglés',
+                    groupValue: selectedLanguage,
+                    activeColor: Color(0xFFEC7A34),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedLanguage = value!;
+                      });
+                    },
+                  ),
+                  RadioListTile<String>(
+                    title: const Text('Francés'),
+                    value: 'Francés',
+                    groupValue: selectedLanguage,
+                    activeColor: Color(0xFFEC7A34),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedLanguage = value!;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      //TODO CAMBIAR IDIOMA
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFEC7A34),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text('Aceptar'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+
+
