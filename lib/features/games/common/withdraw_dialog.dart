011e6ee2fc5d44de884163301eac967dcfae3327
Edison Martinez <ghtdeveloper@gmail.com>
2025-09-28 19:09:13 -0400

    refactor

--- lib/features/games/common/withdraw_dialog.dart
+++ lib/features/games/common/withdraw_dialog.dart
@@ -260,7 +260,7 @@ class _WithdrawalDialogState extends State<WithdrawalDialog> {
             Container(
               padding: EdgeInsets.all(12),
               decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.1),
+                color: Colors.blue.withValues(alpha: 0.1),
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Row(
